eb0ee9d9651abf8d2c473bedd9606599ad26b152
Kangyann <kangyann727@gmail.com>
2025-11-28 12:54:48 +0700

    update: Login Activity

--- app/src/main/java/com/yumify/MainActivity.java
+++ app/src/main/java/com/yumify/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yumify;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    //---
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(view -> {
+            Toast.makeText(this, "Login berhasil!", Toast.LENGTH_SHORT).show();
         });
     }
 }
